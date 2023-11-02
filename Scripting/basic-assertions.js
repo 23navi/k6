bb9a02bef916e7a92f3532bd9cdd5aa623ab1dd1
23navi <navisureka23@gmail.com>
2023-11-02 13:11:58 +0530

    other

--- Scripting/basic-assertions.js
+++ Scripting/basic-assertions.js
@@ -3,7 +3,6 @@ import { check } from "k6";
 
 export default function () {
   const res = http.get("https://test.k6.io");
-  console.log({ res });
   check(res, {
     name_of_assertion: (res) => {
       return res.status === 200;
